fd52b3f9f1ee0953161ec958dddd93bbfe5e9fe7
Dima <dima@gmail.com>
2020-11-18 21:51:55 -0800

    Updated routesForType function

--- src/config/routes/index.js
+++ src/config/routes/index.js
@@ -1,5 +1,5 @@
 import App from 'app'
-import { Common, Developer, Client, Admin, Root, Auth } from 'containers/layout'
+import { Developer, Client, Admin, Root, Auth } from 'containers/layout'
 import { ADMIN_ROOT, AUTH_ROOT, CLIENT_ROOT, DEVELOPER_ROOT, ROOT_PATH } from 'constants/paths'
 import { USER_TYPES } from 'constants/types'
 import publicRoutes from './public'
@@ -10,56 +10,56 @@ import clientRoutes from './client'
 
 
 const routesForType = viewer => ({
-    [USER_TYPES.admin]: [
-        {
-            path: ADMIN_ROOT,
-            component: Admin,
-            routes: adminRoutes(viewer),
-        },
-    ],
-    [USER_TYPES.developer]: [
-        {
-            path: DEVELOPER_ROOT,
-            component: Developer,
-            routes: developerRoutes(viewer)
-        }
-    ],
-    [USER_TYPES.client]: [
-        {
-            path: CLIENT_ROOT,
-            component: Client,
-            routes: clientRoutes(viewer)
-        }
-    ]
-})
+  [USER_TYPES.admin]: [
+    {
+      path: ADMIN_ROOT,
+      component: Admin,
+      routes: adminRoutes(viewer),
+    },
+  ],
+  [USER_TYPES.developer]: [
+    {
+      path: DEVELOPER_ROOT,
+      component: Developer,
+      routes: developerRoutes(viewer)
+    }
+  ],
+  [USER_TYPES.client]: [
+    {
+      path: CLIENT_ROOT,
+      component: Client,
+      routes: clientRoutes(viewer)
+    }
+  ]
+}[viewer.type])
 
 const routes = viewer => {
-    let viewerRouters = []
+  let viewerRouters = []
 
-    if (viewer.isAuthenticated) {
-        viewerRouters = routesForType(viewer)[viewer.type]    
+  if (viewer.isAuthenticated) {
+    viewerRouters = routesForType(viewer)
+  }
+
+  const allRoutes = [
+    ...viewerRouters,
+    {
+      path: AUTH_ROOT,
+      component: Auth,
+      routes: authRoutes(viewer)
+    },
+    {
+      path: ROOT_PATH,
+      component: Root,
+      routes: publicRoutes()
     }
+  ]
 
-    const allRoutes = [
-        ...viewerRouters,
-        {
-            path: AUTH_ROOT,
-            component: Auth,
-            routes: authRoutes(viewer)
-        },
-        {
-            path: ROOT_PATH,
-            component: Root,
-            routes: publicRoutes()
-        }
-    ]
-
-    return [
-        {
-            component: Root,
-            routes: allRoutes
-        }
-    ]
+  return [
+    {
+      component: Root,
+      routes: allRoutes
+    }
+  ]
 }
 
 export default routes
