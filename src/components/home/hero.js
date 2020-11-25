c33de735a8bdf57eb3c6ac05e31101aababa63a8
DESKTOP-CLA0KDN\Xman <ven@hope.com>
2020-11-26 00:39:13 +0800

    [fix] errors from previous version

--- src/components/home/hero.js
+++ src/components/home/hero.js
@@ -11,9 +11,9 @@ const Hero = () => {
         <div className="container">
           <h1>Dedicated development teams that deliver</h1>
           <p>HOPE is a custom software development company that provides dedicated teams of hotshot engineers. We help VC-backed startups and fast-growing tech companies build successful, scalable products that users love.</p>
-          <Button type="danger" shape="round" className="ant-btn-lg">
+          <button className="gradient-button">
             <Link to={AUTH_PATHS.SIGN_UP}>GET STARTED</Link>
-          </Button>
+          </button>
         </div>
       </div>
     </div>
