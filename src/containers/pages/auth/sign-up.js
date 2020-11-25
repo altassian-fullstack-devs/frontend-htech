a8a1637f61efc822bc264a15c024927e9f58bc3b
Star <star@hope.com>
2020-11-25 00:11:42 -0800

    sign up form

--- src/containers/pages/auth/sign-up.js
+++ src/containers/pages/auth/sign-up.js
@@ -1,9 +1,61 @@
 import React from 'react'
+import { Form, Input, Button } from 'antd';
 
-const SignUp = () => (
-  <div>
-    SignUp
-  </div>
-)
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 8 },
+};
+
+const validateMessages = {
+  required: '${label} is required!',
+  types: {
+    email: '${label} is not a valid email!',
+    number: '${label} is not a valid number!',
+  },
+  number: {
+    range: '${label} must be between ${min} and ${max}',
+  },
+};
+
+const SignUp = () => {
+  const onFinish = values => {
+    console.log(values);
+  };
+
+  return (
+    <Form 
+      {...layout}
+      className='auth-form'
+      onFinish={onFinish} 
+      validateMessages={validateMessages}
+    >
+      <Form.Item name={['user', 'name']} label="Name" rules={[{ required: true }]}>
+        <Input />
+      </Form.Item>
+      <Form.Item name={['user', 'email']} label="Email" rules={[{ required: true, type: 'email' }]}>
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please input password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item
+        label="Confirm Password"
+        name="confirm-password"
+        rules={[{ required: true, message: 'Please input confirm password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+        <Button type="primary" htmlType="submit">
+          Sign up
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
 
 export default SignUp
