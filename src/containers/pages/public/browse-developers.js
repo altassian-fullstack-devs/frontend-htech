b7e609332fb50b30abb58049793f98e7eba40b28
Star <star@hope.com>
2020-11-26 05:30:54 -0800

    add pagination to browse developers page

--- src/containers/pages/public/browse-developers.js
+++ src/containers/pages/public/browse-developers.js
@@ -1,7 +1,6 @@
 
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { Layout, Collapse, Typography, List, Avatar, Row, Col } from 'antd'
+import { Layout, Collapse, Typography, List, Avatar, Row, Col, Pagination } from 'antd'
 
 const { Panel } = Collapse
 const { Content, Sider } = Layout
@@ -36,179 +35,183 @@ const SideFilter = () => (
   </Sider>
 )
 
+const data = [
+  {
+    name: 'Ricardo G.',
+    avatar: 'http://gogs.hope.com/avatars/6?s=287',
+    title: 'Cartoonist/Caricaturist/Illustrator',
+    rate: 50,
+    earning: 100000,
+    overview: "20 years of experience in cartoon and caricature creation gives me the skills to meet client's needs. I work in 2 steps: first, a sketch; second - after client approval - digital painting of the sketch.",
+    skills: [
+      'Comit Art',
+      'Digital Painting',
+      'Illustrations',
+      'Caricature Drawing',
+      'Jewelry Design',
+      '3D Modeling',
+      'Interior Design'
+    ]
+  },
+  {
+    name: 'Dakata D.',
+    avatar: 'http://gogs.hope.com/avatars/10?s=287',
+    title: '3D Artist',
+    rate: 30,
+    earning: 500000,
+    overview: "20 years of experience in cartoon and caricature creation gives me the skills to meet client's needs. I work in 2 steps: first, a sketch; second - after client approval - digital painting of the sketch.",
+    skills: [
+      'Comit Art',
+      'Digital Painting',
+      'Illustrations',
+      'Caricature Drawing',
+      'Jewelry Design',
+      '3D Modeling',
+      'Interior Design'
+    ]
+  },
+  {
+    name: 'Alexey M.',
+    avatar: 'http://gogs.hope.com/avatars/9?s=287',
+    title: 'Linux web admin | Ubuntu | VPS | CPanel | Hosting | CMS expert',
+    rate: 30,
+    earning: 100000,
+    overview: "20 years of experience in cartoon and caricature creation gives me the skills to meet client's needs. I work in 2 steps: first, a sketch; second - after client approval - digital painting of the sketch.",
+    skills: [
+      'Comit Art',
+      'Digital Painting',
+      'Illustrations',
+      'Caricature Drawing',
+      'Jewelry Design',
+      '3D Modeling',
+      'Interior Design'
+    ]
+  },
+  {
+    name: 'Konstantis S.',
+    avatar: 'http://gogs.hope.com/avatars/8?s=287',
+    title: 'Video, Audio Production, Graphic Design',
+    rate: 50,
+    earning: 100000,
+    overview: "20 years of experience in cartoon and caricature creation gives me the skills to meet client's needs. I work in 2 steps: first, a sketch; second - after client approval - digital painting of the sketch.",
+    skills: [
+      'Comit Art',
+      'Digital Painting',
+      'Illustrations',
+      'Caricature Drawing',
+      'Jewelry Design',
+      '3D Modeling',
+      'Interior Design'
+    ]
+  },
+  {
+    name: 'Alex I.',
+    avatar: 'http://gogs.hope.com/avatars/7?s=287',
+    title: '2D & 3D Animation, Motion graphic, Video Production',
+    rate: 35,
+    earning: 300000,
+    overview: "20 years of experience in cartoon and caricature creation gives me the skills to meet client's needs. I work in 2 steps: first, a sketch; second - after client approval - digital painting of the sketch.",
+    skills: [
+      'Comit Art',
+      'Digital Painting',
+      'Illustrations',
+      'Caricature Drawing',
+      'Jewelry Design',
+      '3D Modeling',
+      'Interior Design'
+    ]
+  },
+  {
+    name: 'Eason Chow Wal T.',
+    avatar: 'http://gogs.hope.com/avatars/5?s=287',
+    title: 'Industrial Designer, Entrepreneur, Innovator, Maker and Thinker',
+    rate: 80,
+    earning: 20000,
+    overview: "20 years of experience in cartoon and caricature creation gives me the skills to meet client's needs. I work in 2 steps: first, a sketch; second - after client approval - digital painting of the sketch.",
+    skills: [
+      'Comit Art',
+      'Digital Painting',
+      'Illustrations',
+      'Caricature Drawing',
+      'Jewelry Design',
+      '3D Modeling',
+      'Interior Design'
+    ]
+  },
+  {
+    name: 'Claudia D.',
+    avatar: 'http://gogs.hope.com/avatars/4?s=287',
+    title: 'English/Portuguese Translator & Interpreter',
+    rate: 20,
+    earning: 3000,
+    overview: "20 years of experience in cartoon and caricature creation gives me the skills to meet client's needs. I work in 2 steps: first, a sketch; second - after client approval - digital painting of the sketch.",
+    skills: [
+      'Comit Art',
+      'Digital Painting',
+      'Illustrations',
+      'Caricature Drawing',
+      'Jewelry Design',
+      '3D Modeling',
+      'Interior Design'
+    ]
+  },
+  {
+    name: 'Karlen S.',
+    avatar: 'http://gogs.hope.com/avatars/11?s=287',
+    title: 'Master of PS | Photo EDITING and MANIPULATION | High-END Retouching',
+    rate: 50,
+    earning: 40000,
+    overview: "20 years of experience in cartoon and caricature creation gives me the skills to meet client's needs. I work in 2 steps: first, a sketch; second - after client approval - digital painting of the sketch.",
+    skills: [
+      'Comit Art',
+      'Digital Painting',
+      'Illustrations',
+      'Caricature Drawing',
+      'Jewelry Design',
+      '3D Modeling',
+      'Interior Design'
+    ]
+  },
+  {
+    name: 'Eugen L.',
+    avatar: 'http://gogs.hope.com/avatars/3?s=287',
+    title: 'Graphic design/Presentation | Pitch deck design/Web design',
+    rate: 28,
+    earning: 3000,
+    overview: "20 years of experience in cartoon and caricature creation gives me the skills to meet client's needs. I work in 2 steps: first, a sketch; second - after client approval - digital painting of the sketch.",
+    skills: [
+      'Comit Art',
+      'Digital Painting',
+      'Illustrations',
+      'Caricature Drawing',
+      'Jewelry Design',
+      '3D Modeling',
+      'Interior Design'
+    ]
+  },
+  {
+    name: 'Priyanka S.',
+    avatar: 'http://gogs.hope.com/avatars/12?s=287',
+    title: 'Senior Product/Project Manager',
+    rate: 125,
+    earning: 20000,
+    overview: "20 years of experience in cartoon and caricature creation gives me the skills to meet client's needs. I work in 2 steps: first, a sketch; second - after client approval - digital painting of the sketch.",
+    skills: [
+      'Comit Art',
+      'Digital Painting',
+      'Illustrations',
+      'Caricature Drawing',
+      'Jewelry Design',
+      '3D Modeling',
+      'Interior Design'
+    ]
+  }
+];
+
 const BrowseDevelopers = () => {
-  const data = [
-    {
-      name: 'Ricardo G.',
-      avatar: 'http://gogs.hope.com/avatars/6?s=287',
-      title: 'Cartoonist/Caricaturist/Illustrator',
-      rate: 50,
-      earning: 100000,
-      overview: "20 years of experience in cartoon and caricature creation gives me the skills to meet client's needs. I work in 2 steps: first, a sketch; second - after client approval - digital painting of the sketch.",
-      skills: [
-        'Comit Art',
-        'Digital Painting',
-        'Illustrations',
-        'Caricature Drawing',
-        'Jewelry Design',
-        '3D Modeling',
-        'Interior Design'
-      ]
-    },
-    {
-      name: 'Dakata D.',
-      avatar: 'http://gogs.hope.com/avatars/10?s=287',
-      title: '3D Artist',
-      rate: 30,
-      earning: 500000,
-      overview: "20 years of experience in cartoon and caricature creation gives me the skills to meet client's needs. I work in 2 steps: first, a sketch; second - after client approval - digital painting of the sketch.",
-      skills: [
-        'Comit Art',
-        'Digital Painting',
-        'Illustrations',
-        'Caricature Drawing',
-        'Jewelry Design',
-        '3D Modeling',
-        'Interior Design'
-      ]
-    },
-    {
-      name: 'Alexey M.',
-      avatar: 'http://gogs.hope.com/avatars/9?s=287',
-      title: 'Linux web admin | Ubuntu | VPS | CPanel | Hosting | CMS expert',
-      rate: 30,
-      earning: 100000,
-      overview: "20 years of experience in cartoon and caricature creation gives me the skills to meet client's needs. I work in 2 steps: first, a sketch; second - after client approval - digital painting of the sketch.",
-      skills: [
-        'Comit Art',
-        'Digital Painting',
-        'Illustrations',
-        'Caricature Drawing',
-        'Jewelry Design',
-        '3D Modeling',
-        'Interior Design'
-      ]
-    },
-    {
-      name: 'Konstantis S.',
-      avatar: 'http://gogs.hope.com/avatars/8?s=287',
-      title: 'Video, Audio Production, Graphic Design',
-      rate: 50,
-      earning: 100000,
-      overview: "20 years of experience in cartoon and caricature creation gives me the skills to meet client's needs. I work in 2 steps: first, a sketch; second - after client approval - digital painting of the sketch.",
-      skills: [
-        'Comit Art',
-        'Digital Painting',
-        'Illustrations',
-        'Caricature Drawing',
-        'Jewelry Design',
-        '3D Modeling',
-        'Interior Design'
-      ]
-    },
-    {
-      name: 'Alex I.',
-      avatar: 'http://gogs.hope.com/avatars/7?s=287',
-      title: '2D & 3D Animation, Motion graphic, Video Production',
-      rate: 35,
-      earning: 300000,
-      overview: "20 years of experience in cartoon and caricature creation gives me the skills to meet client's needs. I work in 2 steps: first, a sketch; second - after client approval - digital painting of the sketch.",
-      skills: [
-        'Comit Art',
-        'Digital Painting',
-        'Illustrations',
-        'Caricature Drawing',
-        'Jewelry Design',
-        '3D Modeling',
-        'Interior Design'
-      ]
-    },
-    {
-      name: 'Eason Chow Wal T.',
-      avatar: 'http://gogs.hope.com/avatars/5?s=287',
-      title: 'Industrial Designer, Entrepreneur, Innovator, Maker and Thinker',
-      rate: 80,
-      earning: 20000,
-      overview: "20 years of experience in cartoon and caricature creation gives me the skills to meet client's needs. I work in 2 steps: first, a sketch; second - after client approval - digital painting of the sketch.",
-      skills: [
-        'Comit Art',
-        'Digital Painting',
-        'Illustrations',
-        'Caricature Drawing',
-        'Jewelry Design',
-        '3D Modeling',
-        'Interior Design'
-      ]
-    },
-    {
-      name: 'Claudia D.',
-      avatar: 'http://gogs.hope.com/avatars/4?s=287',
-      title: 'English/Portuguese Translator & Interpreter',
-      rate: 20,
-      earning: 3000,
-      overview: "20 years of experience in cartoon and caricature creation gives me the skills to meet client's needs. I work in 2 steps: first, a sketch; second - after client approval - digital painting of the sketch.",
-      skills: [
-        'Comit Art',
-        'Digital Painting',
-        'Illustrations',
-        'Caricature Drawing',
-        'Jewelry Design',
-        '3D Modeling',
-        'Interior Design'
-      ]
-    },
-    {
-      name: 'Karlen S.',
-      avatar: 'http://gogs.hope.com/avatars/11?s=287',
-      title: 'Master of PS | Photo EDITING and MANIPULATION | High-END Retouching',
-      rate: 50,
-      earning: 40000,
-      overview: "20 years of experience in cartoon and caricature creation gives me the skills to meet client's needs. I work in 2 steps: first, a sketch; second - after client approval - digital painting of the sketch.",
-      skills: [
-        'Comit Art',
-        'Digital Painting',
-        'Illustrations',
-        'Caricature Drawing',
-        'Jewelry Design',
-        '3D Modeling',
-        'Interior Design'
-      ]
-    },
-    {
-      name: 'Eugen L.',
-      avatar: 'http://gogs.hope.com/avatars/3?s=287',
-      title: 'Graphic design/Presentation | Pitch deck design/Web design',
-      rate: 28,
-      earning: 3000,
-      overview: "20 years of experience in cartoon and caricature creation gives me the skills to meet client's needs. I work in 2 steps: first, a sketch; second - after client approval - digital painting of the sketch.",
-      skills: [
-        'Comit Art',
-        'Digital Painting',
-        'Illustrations',
-        'Caricature Drawing',
-        'Jewelry Design',
-        '3D Modeling',
-        'Interior Design'
-      ]
-    },
-    {
-      name: 'Priyanka S.',
-      avatar: 'http://gogs.hope.com/avatars/12?s=287',
-      title: 'Senior Product/Project Manager',
-      rate: 125,
-      earning: 20000,
-      overview: "20 years of experience in cartoon and caricature creation gives me the skills to meet client's needs. I work in 2 steps: first, a sketch; second - after client approval - digital painting of the sketch.",
-      skills: [
-        'Comit Art',
-        'Digital Painting',
-        'Illustrations',
-        'Caricature Drawing',
-        'Jewelry Design',
-        '3D Modeling',
-        'Interior Design'
-      ]
-    }
-  ];
+  const onShowSizeChange = (current, pageSize) => {
+    console.log(current, pageSize);
+  }
 
   return (
     <div className="page-browse-developers">
@@ -220,11 +223,6 @@ const BrowseDevelopers = () => {
             dataSource={data}
             renderItem={item => (
               <List.Item className='list-item-developer'>
-                {/* <List.Item.Meta
-                  avatar={<Avatar src="http://gogs.hope.com/avatars/10?s=287" />}
-                  title={<Link to='/profile'>{item.title}</Link>}
-                  description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-                /> */}
                 <Row align='middle' className='row-title'>
                   <Avatar src={item.avatar} size='large' />
                   <Col style={{ marginLeft: 20}} >
@@ -242,6 +240,14 @@ const BrowseDevelopers = () => {
               </List.Item>
             )}
           />
+          <Row className='pagination-container' align='end'>
+            <Pagination
+              showSizeChanger={false}
+              size='small'
+              onShowSizeChange={onShowSizeChange} 
+              defaultCurrent={1} 
+              total={80} />
+          </Row>
         </Content>
       </Layout>
     </div>
