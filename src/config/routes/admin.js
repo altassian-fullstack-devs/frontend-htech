a769be5cd8e60d33bc49a2719ed364cca3e04a2e
Daniel <daniel@hope.com>
2020-11-26 12:27:55 -0800

    add admin route and layout

--- src/config/routes/admin.js
+++ src/config/routes/admin.js
@@ -1,7 +1,17 @@
 import { ADMIN_PATHS } from 'constants/paths'
+import { Clients, Developers } from 'containers/pages/admin'
 
 const routes = () => [
-    
+  {
+    path: ADMIN_PATHS.CLIENTS,
+    extract: true,
+    component: Clients
+  },
+  {
+    path: ADMIN_PATHS.DEVELOPERS,
+    extract: true,
+    component: Developers
+  },
 ]
 
 export default routes
