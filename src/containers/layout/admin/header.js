e6917b25a851eed9d4eb88ab0dbda533803aa87d
Daniel <daniel@hope.com>
2020-11-27 01:45:10 -0800

    update the clients&developers page of admin

--- src/containers/layout/admin/header.js
+++ src/containers/layout/admin/header.js
@@ -1,8 +1,15 @@
 import React from 'react'
-import { Menu, Avatar } from 'antd'
+import { Button, Popover, Avatar } from 'antd'
 import { Link, useLocation } from 'react-router-dom'
 
 import { PUBLIC_PATHS, ROOT_PATH } from 'constants/paths'
+
+const content = (
+  <div>
+    <p><Link to={PUBLIC_PATHS.PROFILE}>Profile</Link></p>
+    <p><Link to={ROOT_PATH}>Sign out</Link></p>
+  </div>
+);
 
 const Navbar = () => {
   const { pathname } = useLocation()
@@ -13,7 +20,9 @@ const Navbar = () => {
         <Link to={ROOT_PATH}>HOPE</Link>
       </div>
       
-      <Avatar className="avatar" src="http://gogs.hope.com/avatars/6?s=287" />
+      <Popover className="header-avatar-popover" placement="bottomRight" content={content} trigger="click">
+        <Button className="header-avatar-button" shape="circle"><Avatar src="http://gogs.hope.com/avatars/6?s=287" /></Button>
+      </Popover>
     </>
   )
 }
